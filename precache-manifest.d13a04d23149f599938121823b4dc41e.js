c7a4da83480e02dfca5c1e39b874814e09bd1549
Bruno <s4ntosbruno@gmail.com>
2020-03-12 12:38:11 -0300

    Updates

---
+++ precache-manifest.d13a04d23149f599938121823b4dc41e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22426db5f48d5e27f3d6fdcf01366e42",
+    "url": "/react-calculadora/index.html"
+  },
+  {
+    "revision": "a956e8fe738442084e18",
+    "url": "/react-calculadora/static/css/main.c5b93ed0.chunk.css"
+  },
+  {
+    "revision": "bc935e03191ca84bb222",
+    "url": "/react-calculadora/static/js/2.a3485c6f.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-calculadora/static/js/2.a3485c6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a956e8fe738442084e18",
+    "url": "/react-calculadora/static/js/main.42641fdb.chunk.js"
+  },
+  {
+    "revision": "87325312c0abe5b59958",
+    "url": "/react-calculadora/static/js/runtime-main.ce547a77.js"
+  },
+  {
+    "revision": "288302ea531af8be59f6ac2b5bbbfdd3",
+    "url": "/react-calculadora/static/media/RobotoMono-Thin.288302ea.ttf"
+  }
+]);
